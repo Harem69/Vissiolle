3d0857174f1ac45f2ba0fd54707e052ff830d05d
Harem69 <74030163+Harem69@users.noreply.github.com>
2021-12-12 15:00:31 -0500

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using VisionImpairedHelper.ViewModels;
+using VisionImpairedHelper.Views;
+using Xamarin.Forms;
+
+namespace VisionImpairedHelper
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+    }
+}
